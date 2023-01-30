b1e98bdff0c8728d019eea0749a5908570478ce8
Luis Guevara <Luis.Guevara@vu.com>
2023-01-30 09:57:57 -0600

    added OT calculations

--- payCalculator.py
+++ payCalculator.py
@@ -1,7 +1,16 @@
 def calculatePay():
     # This first line is provided for you
-    hrs = input("Enter Hours:")
-    
+    hrs = float(input("Enter Hours: "))
+    rate = float(input("Enter Rate: "))
+    pay  = hrs * rate
+    Othours = hrs - 40
+    OtRate = rate * 1.5
+
+    if hrs < 40:
+        print(f"Pay: {pay} ")
+    elif hrs >= 40:
+        pay = (40 * rate ) + (Othours * OtRate)
+        print(f"Pay: {pay} ")
     # end assignment
 
 ## if you want to test locally before you try to sync
