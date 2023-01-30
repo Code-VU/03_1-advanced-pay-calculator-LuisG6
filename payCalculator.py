9ef951148b8ea29848816ac0545669d19676b615
Luis Guevara <Luis.Guevara@vu.com>
2023-01-30 10:11:57 -0600

    updated except code

--- payCalculator.py
+++ payCalculator.py
@@ -1,16 +1,19 @@
 def calculatePay():
     # This first line is provided for you
-    hrs = float(input("Enter Hours: "))
-    rate = float(input("Enter Rate: "))
-    pay  = hrs * rate
-    Othours = hrs - 40
-    OtRate = rate * 1.5
-
-    if hrs < 40:
-        print(f"Pay: {pay} ")
-    elif hrs >= 40:
-        pay = (40 * rate ) + (Othours * OtRate)
-        print(f"Pay: {pay} ")
+    try:    
+        hrs = float(input("Enter Hours: "))
+        rate = float(input("Enter Rate: "))
+        pay  = hrs * rate
+        Othours = hrs - 40
+        OtRate = rate * 1.5
+ 
+        if hrs < 40:
+            print(f"Pay: {pay}")
+        elif hrs >= 40:
+            pay = (40 * rate ) + (Othours * OtRate)
+            print(f"Pay: {pay}")
+    except:
+        print("Error, please enter numeric input")
     # end assignment
 
 ## if you want to test locally before you try to sync
